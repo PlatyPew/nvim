503a06da2d298878b6d8f59e361bac818bfa6ab9
Platy <daryl.limkz@gmail.com>
2025-06-28 10:17:16 +0800

    feat: use native avante instead of native mcphub
    Signed-off-by: Platy <daryl.limkz@gmail.com>

--- lua/plugins/gen-ai.lua
+++ lua/plugins/gen-ai.lua
@@ -117,18 +117,6 @@ return {
                     require("mcphub.extensions.avante").mcp_tool(),
                 }
             end,
-            disabled_tools = {
-                "list_files",
-                "search_files",
-                "read_file",
-                "create_file",
-                "rename_file",
-                "delete_file",
-                "create_dir",
-                "rename_dir",
-                "delete_dir",
-                "bash",
-            },
         },
         config = function(_, opts)
             _G.load_secret_keys({
