5205016b3df9b9bdc110f05ef5f47dda5818079c
Platy <daryl.limkz@gmail.com>
2026-01-25 13:17:50 +0800

    feat: updated to main branch

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,79 +1,13 @@
 return {
     {
-        -- Highlight, edit, and navigate code
         "nvim-treesitter/nvim-treesitter",
-        event = "BufRead",
+        lazy = false,
         build = ":TSUpdate",
-        dependencies = {
-            { "nvim-treesitter/nvim-treesitter-refactor", lazy = true },
-            { "nvim-treesitter/nvim-treesitter-textobjects", lazy = true },
-        },
         init = function()
             vim.wo.foldenable = false
             vim.wo.foldmethod = "expr"
             vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-        end,
-        opts = {
-            auto_install = true,
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },
-            indent = { enable = true },
-            refactor = {
-                highlight_definitions = { enable = true },
-                smart_rename = {
-                    enable = true,
-                    keymaps = {
-                        smart_rename = "gnr",
-                    },
-                },
-                navigation = {
-                    enable = true,
-                    keymaps = {
-                        goto_definition = "gnd",
-                        goto_next_usage = "]]",
-                        goto_previous_usage = "[[",
-                    },
-                },
-            },
-            textobjects = {
-                move = {
-                    enable = true,
-                    set_jumps = true,
-                    goto_next_start = {
-                        ["]f"] = "@function.outer",
-                        ["]o"] = {
-                            query = { "@conditional.outer", "@loop.outer" },
-                        },
-                    },
-                    goto_next_end = {
-                        ["]F"] = "@function.outer",
-                        ["]O"] = {
-                            query = { "@conditional.outer", "@loop.outer" },
-                        },
-                    },
-                    goto_previous_start = {
-                        ["[f"] = "@function.outer",
-                        ["[o"] = {
-                            query = { "@conditional.outer", "@loop.outer" },
-                        },
-                    },
-                    goto_previous_end = {
-                        ["[F"] = "@function.outer",
-                        ["[O"] = {
-                            query = { "@conditional.outer", "@loop.outer" },
-                        },
-                    },
-                },
-            },
-        },
-        config = function(_, opts)
-            if vim.g.install then
-                opts.ensure_installed = "all"
-            end
-
-            require("nvim-treesitter.configs").setup(opts)
+            vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
         end,
     },
 
@@ -100,4 +34,16 @@ return {
         lazy = true,
         event = { "BufReadPost", "BufNewFile" },
     },
+
+    --[[ {
+        "nvim-treesitter/nvim-treesitter-refactor",
+        dependencies = "nvim-treesitter/nvim-treesitter",
+        event = { "BufReadPost", "BufNewFile" },
+    },
+
+    {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        dependencies = "nvim-treesitter/nvim-treesitter",
+        event = { "BufReadPost", "BufNewFile" },
+    }, ]]
 }
